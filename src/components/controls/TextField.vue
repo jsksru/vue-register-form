dc0979980d4e0af216058584553494f0dc469fb4
jsksru <mail@jsks.ru>
2021-02-07 16:34:18 +0500

    textfield component

---
+++ src/components/controls/TextField.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <label>
+    <span>{{title}}</span>
+    <input :type="type" :placeholder="placeholder"/>
+  </label>
+  <span class="error">{{error}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TextField',
+  props: {
+    title: String,
+    placeholder: String,
+    type: String,
+    error: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/vars.scss';
+
+div {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+label {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  span {
+    font-size: $size-normal-text;
+    font-weight: 500;
+    color: $color-text;
+  }
+}
+
+span.error {
+  font-size: $size-small-text;
+  color: $color-danger;
+  min-height: 18px;
+}
+
+input {
+  box-sizing: border-box;
+  border: 1px solid $color-border;
+  border-radius: $size-radius;
+  color: $color-text-active;
+  font-size: $size-normal-text;
+  padding: 16px;
+  outline: none;
+  box-shadow: 0 4px 8px 0px rgba($color-title, 0.04);
+  &::placeholder {
+    color: $color-primary-light;
+  }
+  &:focus {
+    border: 2px solid $color-primary;
+    padding: 15px;
+  }
+}
+</style>

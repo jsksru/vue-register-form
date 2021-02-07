068afdb790550de65b49b6985df29ba3ed79b9c4
jsksru <mail@jsks.ru>
2021-02-07 16:33:55 +0500

    checkbox component

---
+++ src/components/controls/Checkbox.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <label>
+    <input type="checkbox" :checked="checked"/>
+    <span>
+      <slot></slot>
+    </span>
+  </label>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+  props: {
+    checked: Boolean,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/vars.scss';
+</style>

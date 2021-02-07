a7310a717a6e72dcab48808d60253177f3cd1019
jsksru <mail@jsks.ru>
2021-02-07 16:33:17 +0500

    desctiption component

---
+++ src/components/Description.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>
+    <slot></slot>
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/vars.scss';
+
+h2 {
+  margin: 0;
+  font-size: $size-normal-text;
+  font-weight: 400;
+  color: $color-text-active;
+}
+</style>

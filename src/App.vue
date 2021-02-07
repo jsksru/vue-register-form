eeef1f5a41df7274d90dcbf19b8ec8259ae5e113
jsksru <mail@jsks.ru>
2021-02-07 16:32:52 +0500

    app layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <Title>Регистрация</Title>
+  <Description>Уже есть аккаунт? Войти</Description>
+  <form>
+    <div class="form-grid">
+      <TextField title="Имя" placeholder="Введите Ваше имя" type="text"/>
+      <TextField title="Email" placeholder="Введите ваш email" type="email"/>
+      <TextField title="Номер телефона" placeholder="Введите номер телефона" type="tel" error="Введено не корректное значение"/>
+      <Dropdown title="Язык" placeholder="Введите Ваше имя" type="text"/>
+      <Checkbox :checked="false">Принимаю условия использования</Checkbox>
+      <Button type="submit">Зарегистрироваться</Button>
+    </div>
+  </form>
+  
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextField from './components/controls/TextField';
+import Dropdown from './components/controls/Dropdown';
+import Checkbox from './components/controls/Checkbox';
+import Button from './components/controls/Button';
+import Title from './components/Title';
+import Description from './components/Description';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
+    Description,
+    TextField,
+    Dropdown,
+    Checkbox,
+    Button,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import './styles/global.scss';
+
+.form-grid {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 </style>

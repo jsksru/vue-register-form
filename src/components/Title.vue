915bc18d36c4b9141c4d6a7fb49491b1ceeb72aa
jsksru <mail@jsks.ru>
2021-02-07 16:33:26 +0500

    title component

---
+++ src/components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/vars.scss';
+
+h1 {
+  margin: 0;
+  font-size: $size-title-text;
+  font-weight: 600;
+  color: $color-title;
+  margin-bottom: 8px;
+}
+</style>
